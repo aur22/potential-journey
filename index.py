263006be493078dfbd62e79cb2189c39d5dd2565
aur22 <525367725@qq.com>
2025-01-19 16:12:24 +0800

    更新4everland部署配置

--- index.py
+++ index.py
@@ -1,11 +1,12 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_file
 from video_parser import create_app
+import os
 
 app = create_app()
 
 @app.route('/')
 def serve_index():
-    return send_from_directory('templates', 'index.html')
+    return send_file('public/index.html')
 
 if __name__ == "__main__":
-    app.run() 
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000))) 
